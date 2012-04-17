a9b039c7f51ff1578bfd5d5abb4251f61f79b383
Jack Rogers <jack.rogers@ncf.edu>
2012-04-17 16:19:19 -0400

    began skeleton for file widget

--- BioinformaticFileWidget/BFW.py
+++ BioinformaticFileWidget/BFW.py
@@ -5,3 +5,22 @@
 	#take multiple files and merge them into single data table
 	#be able to transpose matrix(switching from samples to genes as entries)
 	#be able to deal with FASTA formats(WHY DOESNT ORANGE DO THIS?)
+	
+	
+	
+
+#fasta reader
+def fastaReader(fastaFile)
+
+	pass
+
+
+
+def transposeMatrix(inputMatrix)	#what structure to use for matrix? array?  npArray?  nested lists?
+
+	pass
+
+
+def concatFiles() #should arguments be directory, list of files, or allow either
+
+	pass
